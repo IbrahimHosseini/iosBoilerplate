ff75f9ac518633d788cc24c68af64c89bf39d875
Ebrahim Hosseini <sehosseini@me.com>
2018-12-30 23:37:30 +0000

    Update baseproject/App/Theme/Colors/Colors.swift, baseproject/App/Theme/Font/Fonts.swift files

--- baseproject/App/Theme/Colors/Colors.swift
+++ baseproject/App/Theme/Colors/Colors.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2018 sehosseini. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class Colors : NSObject {
+    
+    //// Cache
+    
+    private struct Cache {
+        
+        static let baseBlue: UIColor = UIColor(red: 0.110, green: 0.592, blue: 0.745, alpha: 1.000)
+        static let black: UIColor = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1.000)
+        static let gray: UIColor = UIColor(red: 0.490, green: 0.490, blue: 0.490, alpha: 1.000)
+        static let white: UIColor = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1.000)
+        static let red: UIColor = UIColor(red: 0.988, green: 0.063, blue: 0.259, alpha: 1)
+        static let orange: UIColor = UIColor(red: 0.988, green: 0.376, blue: 0.125, alpha: 1)
+        static let darkBlue: UIColor = UIColor(red: 0.012, green: 0.165, blue: 0.294, alpha: 1)
+        static let green: UIColor = UIColor(red: 0.090, green: 0.643, blue: 0.118, alpha: 1)
+        static let tealBlue: UIColor = UIColor(red: 0.447, green: 0.804, blue: 0.918, alpha: 1)
+        static let blue: UIColor = UIColor(red: 0.106, green: 0.655, blue: 0.855, alpha: 1)
+        static let background: UIColor = UIColor(red: 0.490, green: 0.490, blue: 0.490, alpha: 0.05)
+        
+    }
+    
+    //// Colors
+    @objc public dynamic class var baseBlue: UIColor { return Cache.baseBlue}
+    @objc public dynamic class var black: UIColor { return Cache.black }
+    @objc public dynamic class var gray: UIColor { return Cache.gray }
+    @objc public dynamic class var white: UIColor { return Cache.white }
+    @objc public dynamic class var red: UIColor { return Cache.red }
+    @objc public dynamic class var orange: UIColor { return Cache.orange }
+    @objc public dynamic class var darkBlue: UIColor { return Cache.darkBlue }
+    @objc public dynamic class var green: UIColor { return Cache.green }
+    @objc public dynamic class var tealBlue: UIColor { return Cache.tealBlue }
+    @objc public dynamic class var blue: UIColor { return Cache.blue }
+    @objc public dynamic class var background: UIColor { return Cache.background }
+    
+}
--- baseproject/App/Theme/Font/Fonts.swift
+++ baseproject/App/Theme/Font/Fonts.swift
@@ -6,4 +6,410 @@
 //  Copyright © 2018 sehosseini. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+extension UIFont {
+    
+    class func irBoldFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 9.0)
+    }
+    
+    class func irNumBoldFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 9.0)
+    }
+    
+    class func irMediumFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 9.0)
+    }
+    
+    class func irNumMediumFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 9.0)
+    }
+    
+    class func irRegularFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 9.0)
+    }
+    
+    class func irNumRegularFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 9.0)
+    }
+    
+    class func irLightFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 9.0)
+    }
+    
+    class func irUltraLightFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 9.0)
+    }
+    
+    class func irNumLightFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 9.0)
+    }
+    
+    class func irNumUlteraLightFont9() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 9.0)
+    }
+    
+    class func irBoldFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 10.0)
+    }
+    
+    class func irNumBoldFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 10.0)
+    }
+    
+    class func irMediumFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 10.0)
+    }
+    
+    class func irNumMediumFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 10.0)
+    }
+    
+    class func irRegularFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 10.0)
+    }
+    
+    class func irNumRegularFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 10.0)
+    }
+    
+    class func irLightFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 10.0)
+    }
+    
+    class func irUltraLightFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 10.0)
+    }
+    
+    class func irNumLightFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 10.0)
+    }
+    
+    class func irNumUlteraLightFont10() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 10.0)
+    }
+    
+    class func irBoldFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 11.0)
+    }
+    
+    class func irNumBoldFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 11.0)
+    }
+    
+    class func irMediumFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 11.0)
+    }
+    
+    class func irNumMediumFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 11.0)
+    }
+    
+    class func irRegularFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 11.0)
+    }
+    
+    class func irNumRegularFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 11.0)
+    }
+    
+    class func irLightFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 11.0)
+    }
+    
+    class func irUltraLightFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 11.0)
+    }
+    
+    class func irNumLightFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 11.0)
+    }
+    
+    class func irNumUlteraLightFont11() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 11.0)
+    }
+    
+    class func irBoldFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 12.0)
+    }
+    
+    class func irNumBoldFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 12.0)
+    }
+    
+    class func irMediumFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 12.0)
+    }
+    
+    class func irNumMediumFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 12.0)
+    }
+    
+    class func irRegularFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 12.0)
+    }
+    
+    class func irNumRegularFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 12.0)
+    }
+    
+    class func irLightFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 12.0)
+    }
+    
+    class func irUltraLightFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 12.0)
+    }
+    
+    class func irNumLightFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 12.0)
+    }
+    
+    class func irNumUlteraLightFont12() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 12.0)
+    }
+    
+    class func irBoldFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 13.0)
+    }
+    
+    class func irNumBoldFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 13.0)
+    }
+    
+    class func irMediumFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 13.0)
+    }
+    
+    class func irNumMediumFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 13.0)
+    }
+    
+    class func irRegularFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 13.0)
+    }
+    
+    class func irNumRegularFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 13.0)
+    }
+    
+    class func irLightFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 13.0)
+    }
+    
+    class func irUltraLightFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 13.0)
+    }
+    
+    class func irNumLightFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 13.0)
+    }
+    
+    class func irNumUlteraLightFont13() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 13.0)
+    }
+    
+    class func irBoldFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 15.0)
+    }
+    
+    class func irNumBoldFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 15.0)
+    }
+    
+    class func irMediumFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 15.0)
+    }
+    
+    class func irNumMediumFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 15.0)
+    }
+    
+    class func irRegularFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 15.0)
+    }
+    
+    class func irNumRegularFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 15.0)
+    }
+    
+    class func irLightFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 15.0)
+    }
+    
+    class func irUltraLightFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 15.0)
+    }
+    
+    class func irNumLightFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 15.0)
+    }
+    
+    class func irNumUlteraLightFont15() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 15.0)
+    }
+    
+    class func irBoldFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 16.0)
+    }
+    
+    class func irNumBoldFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 16.0)
+    }
+    
+    class func irMediumFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 16.0)
+    }
+    
+    class func irNumMediumFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 16.0)
+    }
+    
+    class func irRegularFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 16.0)
+    }
+    
+    class func irNumRegularFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 16.0)
+    }
+    
+    class func irLightFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 16.0)
+    }
+    
+    class func irUltraLightFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 16.0)
+    }
+    
+    class func irNumLightFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 16.0)
+    }
+    
+    class func irNumUlteraLightFont16() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 16.0)
+    }
+    
+    class func irBoldFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 18.0)
+    }
+    
+    class func irNumBoldFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 18.0)
+    }
+    
+    class func irMediumFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 18.0)
+    }
+    
+    class func irNumMediumFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 18.0)
+    }
+    
+    class func irRegularFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 18.0)
+    }
+    
+    class func irNumRegularFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 18.0)
+    }
+    
+    class func irLightFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 18.0)
+    }
+    
+    class func irUltraLightFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 18.0)
+    }
+    
+    class func irNumLightFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 18.0)
+    }
+    
+    class func irNumUlteraLightFont18() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 18.0)
+    }
+    
+    class func irBoldFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 20.0)
+    }
+    
+    class func irNumBoldFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 20.0)
+    }
+    
+    class func irMediumFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 20.0)
+    }
+    
+    class func irNumMediumFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 20.0)
+    }
+    
+    class func irRegularFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 20.0)
+    }
+    
+    class func irNumRegularFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 20.0)
+    }
+    
+    class func irLightFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 20.0)
+    }
+    
+    class func irUltraLightFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 20.0)
+    }
+    
+    class func irNumLightFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 20.0)
+    }
+    
+    class func irNumUlteraLightFont20() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 20.0)
+    }
+    
+    class func irBoldFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Bold", size: 40.0)
+    }
+    
+    class func irNumBoldFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Bold", size: 40.0)
+    }
+    
+    class func irMediumFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Medium", size: 40.0)
+    }
+    
+    class func irNumMediumFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Medium", size: 40.0)
+    }
+    
+    class func irRegularFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobile", size: 40.0)
+    }
+    
+    class func irNumRegularFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum", size: 40.0)
+    }
+    
+    class func irLightFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-Light", size: 40.0)
+    }
+    
+    class func irUltraLightFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobile-UltraLight", size: 40.0)
+    }
+    
+    class func irNumLightFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-Light", size: 40.0)
+    }
+    
+    class func irNumUlteraLightFont40() -> UIFont? {
+        return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 40.0)
+    }
+}
+
+
