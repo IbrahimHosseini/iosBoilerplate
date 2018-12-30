ae480ada1cfda9c9464cb93f421af253630581e9
Ebrahim Hosseini <sehosseini@me.com>
2018-12-30 23:40:21 +0000

    Update baseproject/App/Libraries/Localize/LocalizeKey.swift

---
+++ baseproject/App/Libraries/Localize/LocalizeKey.swift
@@ -0,0 +1,124 @@
+//
+//  LocalizeKey.swift
+//  Bazargam_Buyer
+//
+//  Created by Seyed Ebrahim Hosseini on 10/9/18.
+//  Copyright © 2018 Bazargam. All rights reserved.
+//
+
+import Foundation
+
+
+/**
+Localizable strings file
+*/
+public let Localizable = "Localizable"
+
+/**
+ "خطای ناشناخته"
+ 
+ All translations:
+ 
+ en: "خطای ناشناخته"
+ 
+ */
+public let HttpCodeUnknown = "HttpCodeUnknown"
+
+/**
+ "خطای ساختاری"
+ 
+ All translations:
+ 
+ en: "خطای ساختاری"
+ 
+ */
+public let FatalError = "FatalError"
+
+/**
+ "سرور در حال بروزرسانی است. کمی بعد تلاش کنید"
+ 
+ All translations:
+ 
+ en: "سرور در حال بروزرسانی است. کمی بعد تلاش کنید"
+ 
+ */
+public let HttpCode502 = "HttpCode502"
+
+/**
+ "درخواست با موفقیت انجام شد."
+ 
+ All translations:
+ 
+ en: "درخواست با موفقیت انجام شد."
+ 
+ */
+public let HttpCode200To300 = "HttpCode200To300"
+
+/**
+ "عدم وجود دسترسی"
+ 
+ All translations:
+ 
+ en: "عدم وجود دسترسی"
+ 
+ */
+public let HttpCode401 = "HttpCode401"
+
+/**
+ "درخواست غیر مجاز"
+ 
+ All translations:
+ 
+ en: "درخواست غیر مجاز"
+ 
+ */
+public let HttpCode403 = "HttpCode403"
+
+/**
+"اطلاعات وارد شده صحیح نمی باشد"
+ 
+ All translations:
+ 
+ en: "اطلاعات وارد شده صحیح نمی باشد"
+ 
+ */
+public let HttpCode404 = "HttpCode404"
+
+/**
+ "The resource that is being accessed is locked"
+ 
+ All translations:
+ 
+ en: "The resource that is being accessed is locked"
+ 
+ */
+public let HttpCode423 = "HttpCode423"
+
+/**
+ "تعدد درخواست لطفا کمی بعد دوباره تلاش نمایید."
+ 
+ All translations:
+ 
+ en: "تعدد درخواست لطفا کمی بعد دوباره تلاش نمایید."
+ 
+ */
+public let HttpCode429 = "HttpCode429"
+
+/**
+ "داده ای برای نمایش موجود نمی باشد."
+ 
+ All translations:
+ 
+ en: "داده ای برای نمایش موجود نمی باشد."
+ 
+ */
+public let NoItemToShow = "NoItemToShow"
+
+/**
+ "%d ریال"
+ 
+ All translations:
+ 
+ en: "%d ریال"
+ 
+ */
