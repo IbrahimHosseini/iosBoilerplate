6e319946e029702d8ffe3fdc9b6e38a6c630497f
Ebrahim Hosseini <sehosseini@me.com>
2019-01-14 13:47:43 +0330

    common mappable

---
+++ baseproject/Model/Common/CommonMappable.swift
@@ -0,0 +1,55 @@
+//
+//  CommonMappable.swift
+//  baseproject
+//
+//  Created by Pazandish on 1/14/19.
+//  Copyright © 2019 sehosseini. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+import SwiftyUserDefaults
+
+public protocol NVMappable: Mappable{
+    init();
+}
+
+extension UserDefaults {
+    public subscript<T>(key: DefaultsKey<T>) -> T? where T:NVMappable {
+        get {
+            if let str = string(forKey: key._key) {
+                return Mapper<T>().map(JSONString: str)
+            }
+            return nil
+        }
+        set {
+            set(key, newValue?.toJSONString())
+            
+        }
+    }
+}
+
+
+extension UserDefaults {
+    public subscript<T:NVMappable>(key: DefaultsKey<T?>) -> T? {
+        get {
+            if let str = string(forKey: key._key){
+                return Mapper<T>().map(JSONString: str)
+            }
+            return nil
+        }
+        set { set(key, newValue?.toJSONString())
+            
+        }
+    }
+    public subscript<T:NVMappable>(key: DefaultsKey<T>) -> T {
+        get {
+            if let str = string(forKey: key._key){
+                return Mapper<T>().map(JSONString: str) ?? T()
+            }
+            return T()
+        }
+        set { set(key, newValue.toJSONString() ?? "") }
+    }
+    
+}
