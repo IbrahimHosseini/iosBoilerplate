cd0036b107472570a684b60f488296bf50693463
Ebrahim Hosseini <sehosseini@me.com>
2019-03-03 09:56:09 +0330

    add quick and nimble test to project

---
+++ UnitTests/ModelTest/LoginTest.swift
@@ -0,0 +1,20 @@
+//
+//  LoginTest.swift
+//  UnitTests
+//
+//  Created by Pazandish on 3/3/19.
+//  Copyright © 2019 sehosseini. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import baseproject
+
+class LoginTest: QuickSpec {
+    override func spec() {
+        print("a")
+        
+        // write test code hire
+        
+    }
+}
