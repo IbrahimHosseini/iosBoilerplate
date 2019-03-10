490ce5317ee87e7d009c6b42bbbc3657ea0635ed
Ebrahim Hosseini <sehosseini@me.com>
2019-03-10 10:38:59 +0330

    modify fonts file

--- baseproject/App/Theme/Font/Fonts.swift
+++ baseproject/App/Theme/Font/Fonts.swift
@@ -410,6 +410,490 @@ extension UIFont {
     class func irNumUlteraLightFont40() -> UIFont? {
         return UIFont(name: "IRANSansMobileFaNum-UltraLight", size: 40.0)
     }
+    
+    class func robotoBlack8() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 8.0)
+    }
+    
+    class func robotoBlack9() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 9.0)
+    }
+    
+    class func robotoBlack10() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 10.0)
+    }
+    
+    class func robotoBlack11() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 11.0)
+    }
+    
+    class func robotoBlack12() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 12.0)
+    }
+    
+    class func robotoBlack13() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 13.0)
+    }
+    
+    class func robotoBlack14() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 14.0)
+    }
+    
+    class func robotoBlack15() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 15.0)
+    }
+    
+    class func robotoBlack18() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 18.0)
+    }
+    
+    class func robotoBlack20() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 20.0)
+    }
+    
+    class func robotoBlack24() -> UIFont? {
+        return UIFont(name: "Roboto-Black", size: 24.0)
+    }
+    
+    class func robotoBold8() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 8.0)
+    }
+    
+    class func robotoBold9() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 9.0)
+    }
+    
+    class func robotoBold10() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 10.0)
+    }
+    
+    class func robotoBold11() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 11.0)
+    }
+    
+    class func robotoBold12() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 12.0)
+    }
+    
+    class func robotoBold13() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 13.0)
+    }
+    
+    class func robotoBold14() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 14.0)
+    }
+    
+    class func robotoBold15() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 15.0)
+    }
+    
+    class func robotoBold18() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 18.0)
+    }
+    
+    class func robotoBold20() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 20.0)
+    }
+    
+    class func robotoBold24() -> UIFont? {
+        return UIFont(name: "Roboto-Bold", size: 24.0)
+    }
+    
+    class func robotoBoldItalic8() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 8.0)
+    }
+    
+    class func robotoBoldItalic9() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 9.0)
+    }
+    
+    class func robotoBoldItalic10() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 10.0)
+    }
+    
+    class func robotoBoldItalic11() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 11.0)
+    }
+    
+    class func robotoBoldItalic12() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 12.0)
+    }
+    
+    class func robotoBoldItalic13() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 13.0)
+    }
+    
+    class func robotoBoldItalic14() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 14.0)
+    }
+    
+    class func robotoBoldItalic15() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 15.0)
+    }
+    
+    class func robotoBoldItalic18() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 18.0)
+    }
+    
+    class func robotoBoldItalic20() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 20.0)
+    }
+    
+    class func robotoBoldItalic24() -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: 24.0)
+    }
+    
+    class func robotoItalic8() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 8.0)
+    }
+    
+    class func robotoItalic9() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 9.0)
+    }
+    
+    class func robotoItalic10() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 10.0)
+    }
+    
+    class func robotoItalic11() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 11.0)
+    }
+    
+    class func robotoItalic12() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 12.0)
+    }
+    
+    class func robotoItalic13() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 13.0)
+    }
+    
+    class func robotoItalic14() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 14.0)
+    }
+    
+    class func robotoItalic15() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 15.0)
+    }
+    
+    class func robotoItalic18() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 18.0)
+    }
+    
+    class func robotoItalic20() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 20.0)
+    }
+    
+    class func robotoItalic24() -> UIFont? {
+        return UIFont(name: "Roboto-Italic", size: 24.0)
+    }
+    
+    class func robotoLight8() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 8.0)
+    }
+    
+    class func robotoLight9() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 9.0)
+    }
+    
+    class func robotoLight10() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 10.0)
+    }
+    
+    class func robotoLight11() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 11.0)
+    }
+    
+    class func robotoLight12() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 12.0)
+    }
+    
+    class func robotoLight13() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 13.0)
+    }
+    
+    class func robotoLight14() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 14.0)
+    }
+    
+    class func robotoLight15() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 15.0)
+    }
+    
+    class func robotoLight18() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 18.0)
+    }
+    
+    class func robotoLight20() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 20.0)
+    }
+    
+    class func robotoLight24() -> UIFont? {
+        return UIFont(name: "Roboto-Light", size: 24.0)
+    }
+    
+    class func robotoLightItalic8() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 8.0)
+    }
+    
+    class func robotoLightItalic9() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 9.0)
+    }
+    
+    class func robotoLightItalic10() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 10.0)
+    }
+    
+    class func robotoLightItalic11() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 11.0)
+    }
+    
+    class func robotoLightItalic12() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 12.0)
+    }
+    
+    class func robotoLightItalic13() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 13.0)
+    }
+    
+    class func robotoLightItalic14() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 14.0)
+    }
+    
+    class func robotoLightItalic15() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 15.0)
+    }
+    
+    class func robotoLightItalic18() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 18.0)
+    }
+    
+    class func robotoLightItalic20() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 20.0)
+    }
+    
+    class func robotoLightItalic24() -> UIFont? {
+        return UIFont(name: "Roboto-LightItalic", size: 24.0)
+    }
+    
+    class func robotoMedium8() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 8.0)
+    }
+    
+    class func robotoMedium9() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 9.0)
+    }
+    
+    class func robotoMedium10() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 10.0)
+    }
+    
+    class func robotoMedium11() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 11.0)
+    }
+    
+    class func robotoMedium12() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 12.0)
+    }
+    
+    class func robotoMedium13() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 13.0)
+    }
+    
+    class func robotoMedium14() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 14.0)
+    }
+    
+    class func robotoMedium15() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 15.0)
+    }
+    
+    class func robotoMedium18() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 18.0)
+    }
+    
+    class func robotoMedium20() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 20.0)
+    }
+    
+    class func robotoMedium24() -> UIFont? {
+        return UIFont(name: "Roboto-Medium", size: 24.0)
+    }
+    
+    class func robotoMediumItalic8() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 8.0)
+    }
+    
+    class func robotoMediumItalic9() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 9.0)
+    }
+    
+    class func robotoMediumItalic10() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 10.0)
+    }
+    
+    class func robotoMediumItalic11() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 11.0)
+    }
+    
+    class func robotoMediumItalic12() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 12.0)
+    }
+    
+    class func robotoMediumItalic13() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 13.0)
+    }
+    
+    class func robotoMediumItalic14() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 14.0)
+    }
+    
+    class func robotoMediumItalic15() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 15.0)
+    }
+    
+    class func robotoMediumItalic18() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 18.0)
+    }
+    
+    class func robotoMediumItalic20() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 20.0)
+    }
+    
+    class func robotoMediumItalic24() -> UIFont? {
+        return UIFont(name: "Roboto-MediumItalic", size: 24.0)
+    }
+    
+    class func robotoRegular8() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 8.0)
+    }
+    
+    class func robotoRegular9() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 9.0)
+    }
+    
+    class func robotoRegular10() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 10.0)
+    }
+    
+    class func robotoRegular11() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 11.0)
+    }
+    
+    class func robotoRegular12() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 12.0)
+    }
+    
+    class func robotoRegular13() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 13.0)
+    }
+    
+    class func robotoRegular14() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 14.0)
+    }
+    
+    class func robotoRegular15() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 15.0)
+    }
+    
+    class func robotoRegular18() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 18.0)
+    }
+    
+    class func robotoRegular20() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 20.0)
+    }
+    
+    class func robotoRegular24() -> UIFont? {
+        return UIFont(name: "Roboto-Regular", size: 24.0)
+    }
+    
+    class func robotoThin8() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 8.0)
+    }
+    
+    class func robotoThin9() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 9.0)
+    }
+    
+    class func robotoThin10() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 10.0)
+    }
+    
+    class func robotoThin11() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 11.0)
+    }
+    
+    class func robotoThin12() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 12.0)
+    }
+    
+    class func robotoThin13() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 13.0)
+    }
+    
+    class func robotoThin14() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 14.0)
+    }
+    
+    class func robotoThin15() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 15.0)
+    }
+    
+    class func robotoThin18() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 18.0)
+    }
+    
+    class func robotoThin20() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 20.0)
+    }
+    
+    class func robotoThin24() -> UIFont? {
+        return UIFont(name: "Roboto-Thin", size: 24.0)
+    }
+    
+    class func robotoThinItalic8() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 8.0)
+    }
+    
+    class func robotoThinItalic9() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 9.0)
+    }
+    
+    class func robotoThinItalic10() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 10.0)
+    }
+    
+    class func robotoThinItalic11() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 11.0)
+    }
+    
+    class func robotoThinItalic12() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 12.0)
+    }
+    
+    class func robotoThinItalic13() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 13.0)
+    }
+    
+    class func robotoThinItalic14() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 14.0)
+    }
+    
+    class func robotoThinItalic15() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 15.0)
+    }
+    
+    class func robotoThinItalic18() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 18.0)
+    }
+    
+    class func robotoThinItalic20() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 20.0)
+    }
+    
+    class func robotoThinItalic24() -> UIFont? {
+        return UIFont(name: "Roboto-ThinItalic", size: 24.0)
+    }
 }
 
 
